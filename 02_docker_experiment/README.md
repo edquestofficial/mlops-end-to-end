e115a580c68b5577b093ceb488b77874dc85eb13
edquest-official <edquestofficial@gmail.com>
2025-05-27 15:53:57 +0530

    readme updated

--- 02_docker_experiment/README.md
+++ 02_docker_experiment/README.md
@@ -1,1 +1,13 @@
 # docker_setup
+
+## Build a Docker Image
+To build docker image use command
+```bash
+ docker build -t salary-api .
+```
+
+## Run a Docker Container
+To run a Docker Container use
+```bash
+ docker run -it --salary_prediction -p 5005:5005 salary-api
+```
--- 02_docker_experiment/linear_regression.py
+++ 02_docker_experiment/linear_regression.py
@@ -3,8 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-import mlflow
-import mlflow.sklearn
+
 
 def load_salary_data():
     df = pd.read_csv("salary_data.csv")
