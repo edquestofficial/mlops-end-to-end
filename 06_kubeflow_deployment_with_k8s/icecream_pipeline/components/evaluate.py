c735318fd6fc4be875a1ac19b5e16ee6be9a35b8
edquest-official <edquestofficial@gmail.com>
2025-06-23 21:35:57 +0530

    added minio creadentials

--- 06_kubeflow_deployment_with_k8s/icecream_pipeline/components/evaluate.py
+++ 06_kubeflow_deployment_with_k8s/icecream_pipeline/components/evaluate.py
@@ -1,12 +1,22 @@
-import pandas as pd
-import joblib
-import json
-import numpy as np
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-
 def evaluate(model_path: str, input_csv_path: str):
     
+    import pandas as pd
+    import joblib
+    import json
+    import numpy as np
+    import mlflow
+    from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+    from sklearn.model_selection import train_test_split
+    import os
+
+    # Inject your MinIO credentials here
+    os.environ["AWS_ACCESS_KEY_ID"] = "YxIAgVvKESgXx4cc1Guj"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "R43tuKQAgwkaPD4t9F0OVTaJo8M66Ce3OltjqCg8"
+    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://10.111.65.79:9000" # minio service ip and port
+    
+    mlflow.set_tracking_uri("http://192.168.1.100:32000")  # Replace with your server IP
+    mlflow.set_experiment("IceCreamPipeline")
+
     df = pd.read_csv(input_csv_path)
     X = df[['temp']]
     y = df['price']
@@ -34,3 +44,10 @@ def evaluate(model_path: str, input_csv_path: str):
 
     print("Evaluation metrics written to /data/eval.json")
     print(metrics)
+
+    # Log to MLflow
+    with mlflow.start_run():
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("mse", mse)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
