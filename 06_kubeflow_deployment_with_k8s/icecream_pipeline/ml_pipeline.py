127e26b51612e1a01ce708e19f695778f3a0d289
edquest-official <edquestofficial@gmail.com>
2025-06-23 21:31:52 +0530

    removed deploy part

--- 06_kubeflow_deployment_with_k8s/icecream_pipeline/ml_pipeline.py
+++ 06_kubeflow_deployment_with_k8s/icecream_pipeline/ml_pipeline.py
@@ -4,16 +4,15 @@ from kfp.components import create_component_from_func
 from components.preprocess import preprocess
 from components.train import train
 from components.evaluate import evaluate
-# from components.deploy import deploy
 
-preprocess_op = create_component_from_func(preprocess, base_image="sklearn:v2")
-train_op = create_component_from_func(train, base_image="sklearn:v2")
-evaluate_op = create_component_from_func(evaluate, base_image="sklearn:v2")
-# deploy_op = create_component_from_func(deploy, base_image="sklearn:v2")
+
+preprocess_op = create_component_from_func(preprocess, base_image="sklearn:v1")
+train_op = create_component_from_func(train, base_image="sklearn:v1")
+evaluate_op = create_component_from_func(evaluate, base_image="sklearn:v1")
 
 @dsl.pipeline(
-    name="Ice Cream Pipeline (Advanced)",
-    description="Includes metrics, volume sharing, and S3 readiness"
+    name="Ice Cream Pipeline",
+    description="Includes metrics, and volume sharing."
 )
 def ice_cream_pipeline(test_size: float = 0.2, random_state: int = 42):
 
@@ -38,9 +37,6 @@ def ice_cream_pipeline(test_size: float = 0.2, random_state: int = 42):
         input_csv_path="/data/icecream_clean.csv"
     ).after(train_step)
     evaluate_step.add_pvolumes({"/data": train_step.pvolume})
-
-#     # Step 4: Deploy
-#     deploy_step = deploy_op(model_input=train_step.outputs["model_output"]).after(evaluate_step)
 
 # Compile pipeline
 if __name__ == "__main__":
